bd0431c6e5f52a0283fc7b0ec0833e657a7949f3
AfnanSSa <iafnanz67@gmail.com>
2024-04-07 10:09:27 +0400

    Added attributes and methods

--- InvoiceSystem/Item.java
+++ InvoiceSystem/Item.java
@@ -1,4 +1,83 @@
 package InvoiceSystem;
 
+import java.util.Objects;
+
 public class Item {
+    private Integer id; //class ID
+    private Integer itemID; //item ID
+    private String name; //item name
+    private Double unitPrice;
+    private Integer quantity;
+
+    //method to calculate total amount
+    public Double totalAmount(Integer quantity, Double unitPrice){
+        return quantity * unitPrice;
+    }
+
+    //Getters & Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getItemID() {
+        return itemID;
+    }
+
+    public void setItemID(Integer itemID) {
+        this.itemID = itemID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(Double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    //equals()
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return Objects.equals(itemID, item.itemID);
+    }
+
+    //hashCode()
+    @Override
+    public int hashCode() {
+        return Objects.hash(itemID);
+    }
+
+    //toString()
+    @Override
+    public String toString() {
+        return "Item{" +
+                "itemID=" + itemID +
+                ", name='" + name + '\'' +
+                ", unitPrice=" + unitPrice +
+                ", quantity=" + quantity +
+                '}';
+    }
 }
