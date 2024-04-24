ad3a7060a272e58eda9c27beb1de6e9ce5ab641f
AfnanSSa <iafnanz67@gmail.com>
2024-04-24 09:31:37 +0400

    Updated createNewInvoice() functionality
    Updated createNewInvoice() functionality and added methode that are related to creating new invoice such as findingElementByID() and displayAllItems().

--- InvoiceSystem/InvoicingSystem.java
+++ InvoiceSystem/InvoicingSystem.java
@@ -11,14 +11,15 @@ public class InvoicingSystem {
     //Lists to store invoiced and items
     private static List<Item> items = new ArrayList<>();
     private static List<Invoice> invoices = new ArrayList<>();
+    public static Invoice newInvoice;
 
     public static void main(String[] args) {
         Boolean exit = Boolean.FALSE;
-        while (!exit){
+        while (!exit) {
             //display main menu from Menu class
             Integer option = mainMenu.displayMainMenu();
 
-            switch (option){
+            switch (option) {
                 case 1:
                     handleShopSetting();
                     break;
@@ -27,7 +28,7 @@ public class InvoicingSystem {
                     break;
                 case 3:
                     createInvoice();
-                     break;
+                    break;
                 case 4:
                     reportItemStatistics();
                     break;
@@ -53,14 +54,14 @@ public class InvoicingSystem {
     }
 
     //method to handle option 1 (Shop Settings)
-    private static void handleShopSetting(){
+    private static void handleShopSetting() {
         Boolean back = Boolean.FALSE;
 
-        while (!back){
+        while (!back) {
 
             Integer option = mainMenu.displayShopSettingsMenu();
 
-            switch (option){
+            switch (option) {
                 case 1:
                     loadData(); //implementing Load Data functionality
                     break;
@@ -80,21 +81,21 @@ public class InvoicingSystem {
     }
 
     //method to load data (option 1 in Shop Settings Menu)
-    private static void loadData(){
+    private static void loadData() {
         System.out.println("Loading Data . .");
         //action of loading data
 
     }
 
     //method to set shop name (option 2 in Shop Settings Menu)
-    private static void setShopName(){
+    private static void setShopName() {
         System.out.println("\nEnter new shop name: ");
         String newShopName = scanner.nextLine();
         System.out.println("Shop Name: " + newShopName); //setting the new name
     }
 
     //method to set invoice header (option 3 in Shop Settings Menu)
-    private static void setInvoiceHeader(){
+    private static void setInvoiceHeader() {
         System.out.println("\nEnter new invoice header:");
         System.out.print("Tel: ");
         String telNumber = scanner.nextLine();
@@ -106,19 +107,19 @@ public class InvoicingSystem {
         String website = scanner.nextLine();
         //setting shop header
         System.out.println("\nInvoice header set to: Tel: " +
-                            telNumber + " | Fax: " + fax + " | Email " +
-                            email + " | Website: " + website);
+                telNumber + " | Fax: " + fax + " | Email " +
+                email + " | Website: " + website);
     }
 
     //method to handle option 2 in Main Menu (Manage Shop Items)
-    private static void handleManageItems(){
+    private static void handleManageItems() {
         Boolean back = Boolean.FALSE;
 
-        while (!back){
+        while (!back) {
 
             Integer option = mainMenu.displayManageItemsMenu();
 
-            switch (option){
+            switch (option) {
                 case 1:
                     addItem(); //adding item
                     break;
@@ -141,7 +142,7 @@ public class InvoicingSystem {
     }
 
     //method to add items (option 1 in Manage Items Menu)
-    private static void addItem(){
+    private static void addItem() {
         System.out.println("Adding Item . .");
 
         //asking user to enter item details
@@ -163,7 +164,7 @@ public class InvoicingSystem {
     }
 
     //method to delete items (option 2 in Manage Items Menu)
-    private static void deleteItem(){
+    private static void deleteItem() {
         System.out.println("Deleting Item . .");
         //asking user to select and delete an item from the items list
         Scanner scanner = new Scanner(System.in);
@@ -174,23 +175,23 @@ public class InvoicingSystem {
         Boolean isFound = Boolean.FALSE;
         Item itemToRemove = null;
 
-        for (Item item : items){
-            if (item != null && item.getItemID() != null && item.getItemID().equals(deleteItem)){
+        for (Item item : items) {
+            if (item != null && item.getItemID() != null && item.getItemID().equals(deleteItem)) {
                 itemToRemove = item;
                 isFound = Boolean.TRUE;
                 break;
             }
         }
-        if (isFound){
+        if (isFound) {
             items.remove(itemToRemove);
             System.out.println("Item deleted successfully");
-        }else {
+        } else {
             System.out.println("Item with ID " + deleteItem + " not found");
         }
     }
 
     //method to change item price (option 3 in Manage Items Menu)
-    private static void changeItemPrice(){
+    private static void changeItemPrice() {
         System.out.println("Updating Price . .");
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter item ID to update the price: ");
@@ -199,8 +200,8 @@ public class InvoicingSystem {
         Boolean isFound = Boolean.FALSE;
 
 
-        for (Item item : items){
-            if (item.getItemID().equals(updatePrice)){
+        for (Item item : items) {
+            if (item.getItemID().equals(updatePrice)) {
                 System.out.println("Enter new price: ");
                 Double newPrice = scanner.nextDouble();
 
@@ -212,31 +213,33 @@ public class InvoicingSystem {
                 break;
             }
         }
-        if (!isFound){
+        if (!isFound) {
             System.out.println("Item with ID " + updatePrice + " not found");
         }
 
     }
 
     //method to report all items (option 4 in Manage Items Menu)
-    private static void reportAllItems(){
-        System.out.println("Reporting All Items . .");
+    private static void reportAllItems() {
+        System.out.println("Displaying All Items . .");
         //displaying report of all items in items list
-        if (items.isEmpty()){
+        if (items.isEmpty()) {
             System.out.println("No items available");
         } else {
             System.out.println("List of items: ");
-            for (Item item : items){
+            for (Item item : items) {
                 System.out.println(item);
             }
         }
     }
 
     //method to handle option 3 in Main Menu (create nwe invoice)
-    private static void createInvoice(){
-        Invoice invoice = new Invoice();
+    private static void createInvoice() {
         System.out.println("Creating New Invoice . .");
         Scanner scanner = new Scanner(System.in);
+        Invoice invoice = new Invoice();
+        List<Item> selectedItems = new ArrayList<>();
+
 
         //asking user to input invoice details
         System.out.print("Enter invoice ID: ");
@@ -249,68 +252,118 @@ public class InvoicingSystem {
         System.out.print("Enter invoice date (e.g., DD-MM-YYYY): ");
         String date = scanner.nextLine();
 
-        //create a list to store items
-        List<Item> itemList = new ArrayList<>();
+        if (items.isEmpty()) { //no items in list
+            System.out.println("No Items Available");
+        } else {
+            //showing list of items
+            System.out.println("Available Items:");
+            displayAllItems();
 
-        //showing items from the items list
-        for (Item item : items){
-            System.out.println("ID   |    Name    |  Qty. |   Unit price   |    Total");
-            System.out.println(item.getItemID() + "    |    "+ item.getName() + "     |  " +
-                    item.getQuantity() + "  |   " + item.getUnitPrice()+ "    |    "+
-                    item.totalAmount(item.getQuantity(), item.getUnitPrice()));
+            while (true) {
+                //prompting user to select item ID to add it to the invoice
+                System.out.println("Enter Item ID to add it to the invoice (press 0 to quit): ");
 
-            //create new object for the invoice
-            Item newItem = new Item(item.getItemID(), item.getName(), item.getUnitPrice(), item.getQuantity());
-            itemList.add(newItem);
+                Integer itemID = scanner.nextInt();
+                if (itemID == 0) {
+                    break; //exit while loop
+                }
+
+                Item itemToBeAdded = findItemByID(itemID); //finding to item by its ID
+                if (itemToBeAdded != null) {
+                    selectedItems.add(itemToBeAdded); //adding the item to selected items list
+                    System.out.println("Item: " + itemToBeAdded.getName() + "is added to the invoice");
+                } else {
+                    System.out.println("Item not found");
+                }
+            }
+
+            //creating and adding the invoice to the list of invoices
+            newInvoice = new Invoice(invoiceID, customerName, phoneNumber, date, selectedItems, invoice.totalAmount(selectedItems));
+
+            invoices.add(newInvoice);
+            System.out.println("Invoice is created successfully");
+
+            //printing the invoice
+            System.out.println("\nInvoice Details:");
+            System.out.println("-------------------------------------------");
+            System.out.println("Item ID\tItem Name\tQuantity\tUnit Price\tTotal\tAmount");
+            for (Item item : selectedItems) {
+                System.out.println(item.getItemID() + " \t\t " + item.getName() + " \t\t " +
+                        item.getQuantity() + " \t\t " + item.getUnitPrice() + " \t\t " + item.totalAmount(item.getQuantity(), item.getUnitPrice()));
+            }
+            System.out.println("Total Amount: " + newInvoice.totalAmount(selectedItems));
         }
-        //displaying the result of calculating the total amount
-        System.out.println("Total Amount: " + invoice.totalAmount(itemList));
+    }
 
-        //creating new invoice object with gathered details
-        Invoice newInvoice = new Invoice(invoiceID, customerName, phoneNumber,date, itemList, invoice.totalAmount(itemList));
+    //method to find item by ID
+    private static Item findItemByID(Integer itemID) {
+        for (Item item : items) {
+            if (item.getItemID() == itemID) {
+                return item;
+            }
+        }
+        return null; //if item is not found
+    }
 
-        //adding the new invoice to the invoices list
-        invoices.add(newInvoice);
+    //method to display items
+    private static void displayAllItems() {
+        for (Item item : items) {
+            System.out.println(
+                    "Item ID: " + item.getItemID() +
+                            "| Name: " + item.getName() +
+                            "| Unit Price: $" + item.getUnitPrice() +
+                            "| Quantity: " + item.getQuantity()
+            );
+        }
+    }
 
-        System.out.println("Invoice created successfully");
+    //method to print invoice
+    private static void printInvoice(Invoice invoice) {
+        System.out.println("Invoice ID: " + invoice.getInvoiceID() +
+                "\nCostumer Name: " + invoice.getCostumerName() +
+                "\nCustomer Phone No.: " + invoice.getPhoneNumber() +
+                "\nDate pf invoice:" + invoice.getDate() +
+                "\nItem Id");
+
     }
 
     //method to handle option 4 in Main Menu (Report Item Statistics)
-    private static void reportItemStatistics(){
+    private static void reportItemStatistics() {
         System.out.println("Displaying statistics...");
         System.out.println("Number of items: " + items.size());
         System.out.println("Number of invoices: " + invoices.size());
 
         //calculating the total sales
         Double totalSales = 0.0;
-        for (Invoice invoice : invoices){
-            totalSales +=  invoice.getTotalAmount();
+        for (Invoice invoice : invoices) {
+            totalSales += invoice.getTotalAmount();
         }
 
         //displaying total sales
         System.out.println("Total sales: $" + totalSales);
     }
+
     //method to handle option 5 in Main Menu (Report All Invoices)
-    private static void reportAllInvoices(){
+    private static void reportAllInvoices() {
         System.out.println("Displaying All Invoices...");
         //action of displaying report
     }
 
     //method to handle option 6 in Main Menu (Search Invoices)
-    private static void searchInvoices(){
+    private static void searchInvoices() {
         System.out.println("Searching Invoices . .");
         //asking user to enter ID of invoice
         //action of search
     }
 
     //method to handle option 7 in Main Menu (Program Statistics)
-    private static void handleProgramStatistics(){
+    private static void handleProgramStatistics() {
         System.out.println("Displaying Program Statistics...");
         //action of displaying program statistics
     }
 
     //method to handle option 8 in Main Menu (Exit)
-    private static Boolean confirmExit(){
+    private static Boolean confirmExit() {
         System.out.print("Are you sure you want to exit? (yes/no): ");
         String confirmOption = scanner.nextLine();
         return confirmOption.equalsIgnoreCase("yes");
